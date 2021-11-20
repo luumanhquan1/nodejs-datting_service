fb636094115ef6d1c65bf729d22c5008e72457b6
luumanhquan1 <luumanhquan2001@gmail.com>
2021-11-21 02:02:32 +0700

    deloy heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT=process.env.PORT||3000;
+
 
 app.use(express.json());
 app.use("/api",require("./src/routers/api_router/api_router"));
@@ -10,6 +10,6 @@ app.use("/admin",require("./src/routers/admin/admin_router"));
 app.use('/auth',require("./src/routers/auth_router/auth_router"));
 
 
-app.listen(PORT, function () {
+app.listen(process.env.PORT, function () {
   console.log("listen port 3000");
 })
--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,15 +1,15 @@
 
 const mySql=require('mysql');
-const connect= mySql.createConnection({
+let connect;
+ connect= mySql.createConnection({
     host:'localhost',
     user:'root',
     password:'',
     database:'dattingapp',
-    port:"3306"
 });
 connect.connect(function (err) {
     if(!!err){
-        console.log(err);
+      
     }else{
         console.log('connect');
     }
