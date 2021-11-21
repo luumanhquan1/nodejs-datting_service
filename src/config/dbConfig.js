83dd7d20139aad628ce383abc04691fc0d6507b2
luumanhquan1 <luumanhquan2001@gmail.com>
2021-11-21 11:30:05 +0700

    deloy mysql

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -2,7 +2,12 @@
 const mySql=require('mysql');
 let connect;
 if(process.env.CLEARDB_DATABASE_URL){
-    connect= mySql.createConnection(process.env.CLEARDB_DATABASE_URL);
+    connect= mySql.createConnection({
+        host:'us-cdbr-east-04.cleardb.com',
+        user:'b3ce2acda04ae7',
+        password:'76536e54',
+        database:'heroku_e712c9993ee96b6',
+    });
 }else{
  connect= mySql.createConnection({
     host:'localhost',
