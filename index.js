bcd99a163a4426858d7b500f845a3f53394e2610
luumanhquan1 <luumanhquan2001@gmail.com>
2021-11-21 15:29:10 +0700

    delout

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const  connect  = require('./src/routers/api_router/api_router');
 const app = express();
 
 var Port=process.env.PORT||3000;
 app.use(express.json());
+app.use(function(req,res,next){
+ connect.connect(function (err) {
+    if(!!err){
+      console.log(err);
+    }else{
+        console.log('connect');
+    }
+});
+next();
+});
 app.use("/api",require("./src/routers/api_router/api_router"));
 
 app.use("/admin",require("./src/routers/admin/admin_router"));
--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -7,6 +7,15 @@ connect= mySql.createConnection({
     password:'76536e54',
     database:'heroku_e712c9993ee96b6',
 });
+connect.on('error',(value)=>{
+    console.log("2321");
+    connect= mySql.createConnection({
+        host:'us-cdbr-east-04.cleardb.com',
+        user:'b3ce2acda04ae7',
+        password:'76536e54',
+        database:'heroku_e712c9993ee96b6',
+    });
+});
 // connect.connect(function (err) {
 //     if(!!err){
 //       console.log(err);
