3872f3a9aae484a769e1df6a49c3bf43f1d3743f
luumanhquan1 <luumanhquan2001@gmail.com>
2021-11-21 11:18:03 +0700

    deloy mysql

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-
+var Port=process.env.PORT||3000;
 app.use(express.json());
 app.use("/api",require("./src/routers/api_router/api_router"));
 
@@ -10,6 +10,6 @@ app.use("/admin",require("./src/routers/admin/admin_router"));
 app.use('/auth',require("./src/routers/auth_router/auth_router"));
 
 
-app.listen(process.env.PORT, function () {
-  console.log("listen port 3000");
+app.listen(Port, function () {
+  console.log("listen port "+Port);
 })
--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,15 +1,19 @@
 
 const mySql=require('mysql');
 let connect;
+if(process.env.CLEARDB_DATABASE_URL){
+    connect= mySql.createConnection(process.env.CLEARDB_DATABASE_URL);
+}else{
  connect= mySql.createConnection({
     host:'localhost',
     user:'root',
     password:'',
     database:'dattingapp',
 });
+}
 connect.connect(function (err) {
     if(!!err){
-      
+      console.log(err);
     }else{
         console.log('connect');
     }
