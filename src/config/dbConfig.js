0f054827d5496f8a8f2065df2a4642b8d9c0a3bb
luumanhquan1 <luumanhquan2001@gmail.com>
2021-11-21 11:48:18 +0700

    deloy database

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,16 +1,12 @@
 
 const mySql=require('mysql');
 let connect;
-if(process.env.CLEARDB_DATABASE_URL){
-    connect= mySql.createConnection(process.env.CLEARDB_DATABASE_URL);
-}else{
- connect= mySql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'dattingapp',
+connect= mySql.createConnection({
+    host:'us-cdbr-east-04.cleardb.com',
+    user:'b3ce2acda04ae7',
+    password:'76536e54',
+    database:'heroku_e712c9993ee96b6',
 });
-}
 connect.connect(function (err) {
     if(!!err){
       console.log(err);
