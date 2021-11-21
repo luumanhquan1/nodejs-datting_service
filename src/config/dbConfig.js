ab2af43af2d81cbac02e02dd475783b953ba0089
luumanhquan1 <luumanhquan2001@gmail.com>
2021-11-21 14:30:11 +0700

    ...

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,12 +1,15 @@
 
 const mySql=require('mysql');
 let connect;
-connect= mySql.createConnection({
-    host:'us-cdbr-east-04.cleardb.com',
-    user:'b3ce2acda04ae7',
-    password:'76536e54',
-    database:'heroku_e712c9993ee96b6',
-});
+function connectDb(){
+    connect= mySql.createConnection({
+        host:'us-cdbr-east-04.cleardb.com',
+        user:'b3ce2acda04ae7',
+        password:'76536e54',
+        database:'heroku_e712c9993ee96b6',
+    });
+    connect.on('error',connectDb());
+}
 // connect.connect(function (err) {
 //     if(!!err){
 //       console.log(err);
